987d129bfa51c96e8e07e786191f202bcacdee11
chrisjdavie <chris.d@theasi.co>
2016-01-10 18:27:13 +0000

    chicken is tidier

--- mapsProto/js/map.js
+++ mapsProto/js/map.js
@@ -1,4 +1,4 @@
-var map, pointarray, heatmap;
+var map, pointarray, heatmap, gradient;
 
 function initMap() {
     /* Data points defined as a mixture of WeightedLocation and LatLng objects */
@@ -16,20 +16,58 @@ function initMap() {
 
         map = new google.maps.Map(document.getElementById('map'), {
           center: london,
-          zoom: 13,
+          zoom: 11,
 //          mapTypeId: google.maps.MapTypeId.SATELLITE
         });
 
-        var heatmap = new google.maps.visualization.HeatmapLayer({
+        heatmap = new google.maps.visualization.HeatmapLayer({
           data: heatMapData,
 		  radius: 15*Math.pow(2,map.getZoom()-11)
         });
         heatmap.setMap(map);
+        changeGradient();
+        setLegendGradient();
+        setLegendLabels();
         
-
         google.maps.event.addListener(map, 'zoom_changed', function () {
-          heatmap.setOptions({radius:15*Math.pow(2,map.getZoom()-11)});
-        });      
-          
+            heatmap.setOptions({radius:15*Math.pow(2,map.getZoom()-11)});
+        });
+        
     });
 }
+
+function changeGradient() {
+  gradient = [
+    'rgba(0, 0, 0, 0)',
+    'rgba(109, 83, 16, 0.6)',
+    'rgba(174,132, 26, 0.8)',
+    'rgba(196,149, 28, 0.9)',
+    'rgba(218,165, 32, 1)'
+  ]
+  heatmap.set('gradient', heatmap.get('gradient') ? null : gradient);
+}
+
+function setLegendGradient() {
+    var gradientCss = '(left';
+    for (var i = 0; i < gradient.length; ++i) {
+        gradientCss += ', ' + gradient[i];
+    }
+    gradientCss += ')';
+    
+    $('#legendGradient').css('background', '-webkit-linear-gradient' + gradientCss);
+    $('#legendGradient').css('background', '-moz-linear-gradient' + gradientCss);
+    $('#legendGradient').css('background', '-o-linear-gradient' + gradientCss);
+    $('#legendGradient').css('background', 'linear-gradient' + gradientCss);
+}
+
+function setLegendLabels() {
+        var legendWidth = $('#legendGradient').outerWidth();
+          $('#legend').append($('<div>').css({
+            'position': 'absolute',
+            'right': 0 + 'px',
+            'top': '18px',
+            'text-align': 'center'
+        }).html('More chicken'));  
+}
+
+
