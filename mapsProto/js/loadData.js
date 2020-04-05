0ae72fce9696e059986bae96fcbf45f931709f70
Chris Davie <cjdavie@googlemail.com>
2020-04-05 17:47:18 +0100

    clean up old files

--- mapsProto/js/loadData.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-    $.getJSON("js/sfDataJson.js", function(data){
-        var Append = "test";
-        for (var i = 0; i < data.tableData.length; i++) {
-            Append += data.tableData[i].lat + " ";
-        }
-        $("div").append(Append);
-    });
-});
