8da2ab485906a44fd48e5f4bf3ba2ffbecd54c0a
Chris Davie <cjdavie@googlemail.com>
2021-01-24 16:12:35 +0000

    bar charts fully js, some text improvements

--- blog/tech_debt_js_graphs/bar_chart.js
+++ blog/tech_debt_js_graphs/bar_chart.js
@@ -1,32 +1,46 @@
-function draw_chart() {
+function draw_chart(
+    plot_id,
+    after_label,
+    other = null,
+    code_debt = null,
+    new_work = null,
+    servers = null) {
+
     var patternSize = 20;
-    var barChartData = {
-        labels: ['Before', 'After (Expected)'],
-        datasets: [{
+    var datasets = [];
+    if (other) {
+        datasets.push({
             label: 'Other',
             backgroundColor: pattern.draw('diagonal', 'rgba(31, 119, 180)'), //',
-            data: [
-                40,
-                40
-            ]
-        }, {
-            label: 'Code debt',
+            data: other
+        })
+    }
+    if (servers) {
+        datasets.push({
+            label: 'Servers',
+            backgroundColor: pattern.draw('diamond', 'rgba(255, 127, 14)'),
+            data: servers
+        })
+    }
+    if (code_debt) {
+        datasets.push({
+            label: 'Code problems',
             backgroundColor: pattern.draw('zigzag-vertical', 'rgba(44, 160, 44)'),
-            data: [
-                50,
-                5
-            ]
-        }, {
+            data: code_debt
+        })
+    }
+    if (new_work) {
+        datasets.push({
             label: 'New Work',
             backgroundColor: pattern.draw('diagonal-right-left', 'rgba(214, 39, 40)'),
-            data: [
-                10,
-                55
-            ]
-        }]
-
+            data: new_work
+        })
+    }
+    var barChartData = {
+        labels: ['Before', after_label],
+        datasets: datasets
     };
-    var ctx = document.getElementById('myChart').getContext('2d');
+    var ctx = document.getElementById(plot_id).getContext('2d');
     var body = document.body;
     var style = window.getComputedStyle(body, null).getPropertyValue('font-size');
     var fontSize = parseFloat(style);
